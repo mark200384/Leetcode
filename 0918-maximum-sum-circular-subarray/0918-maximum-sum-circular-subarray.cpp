0511dd76e6c2cb1d23661ca82fbb988d2c0d6a4e
Yen <61017020+mark200384@users.noreply.github.com>
2023-11-29 12:50:38 +0800

    Time: 49 ms (64.17%), Space: 40.2 MB (68.82%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        const int n = nums.size();
-        vector<int> rightMax(n);
-        rightMax[n - 1] = nums[n - 1];
-        int suffixSum = nums[n - 1];
-
-        for (int i = n - 2; i >= 0; --i) {
-            suffixSum += nums[i];
-            rightMax[i] = max(rightMax[i + 1], suffixSum);
-        }
-
-        int maxSum = nums[0];
-        int specialSum = nums[0];
         int curMax = 0;
-        int prefixSum = 0;
-        for (int i = 0; i < n; ++i) {
-            // This is Kadane's algorithm.
-            curMax = max(curMax, 0) + nums[i];
+        int curMin = 0;
+        int maxSum = nums[0];
+        int minSum = nums[0];
+        int totalSum = 0;
+        
+        for (int num: nums) {
+            // Normal Kadane's
+            curMax = max(curMax, 0) + num;
             maxSum = max(maxSum, curMax);
-
-            prefixSum += nums[i];
-            if (i + 1 < n) {
-                specialSum = max(specialSum, prefixSum + rightMax[i + 1]);
-            }
+            
+            // Kadane's but with min to find minimum subarray
+            curMin = min(curMin, 0) + num;
+            minSum = min(minSum, curMin);
+            
+            totalSum += num;  
         }
 
-        return max(maxSum, specialSum);
+        if (totalSum == minSum) {
+            return maxSum;
+        }
+        
+        return max(maxSum, totalSum - minSum);
     }
 };
