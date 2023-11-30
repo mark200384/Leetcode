6ad524c60f4285f10ae8232a41a24a73ff2b61ce
Yen <61017020+mark200384@users.noreply.github.com>
2023-11-30 15:25:14 +0800

    Time: 3 ms (53.58%), Space: 6.9 MB (92.29%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    //dp[i] represents the number of combinations to make a sum of i.
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target+1, 0);
+        dp[0] = 1;
+        for(int i=1; i<=target; i++) {
+            for(int j=0; j<nums.size(); j++) {
+                if(i>= nums[j]) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
