b7e6cf937f71e1902e09274c82fb2030d3551474
Yen <61017020+mark200384@users.noreply.github.com>
2023-12-01 23:27:16 +0800

    Time: 2 ms (46.09%), Space: 6.5 MB (32.75%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1 || n==2) return n;
+        vector<int> res(n+1, 0);
+        res[1] = 1, res[2] = 2;
+        for(int i=3; i<=n; i++) {
+            res[i] = res[i-1] + res[i-2];
+        }
+        return res[n];
+    }
+};
