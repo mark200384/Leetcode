6a452a4d4a98d84aa965ca4b300ca1292e0906e5
Yen <61017020+mark200384@users.noreply.github.com>
2023-12-03 01:03:24 +0800

    Time: 70 ms (60.18%), Space: 14.9 MB (55.00%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    //f(n) = Min( 1 + f(n- c1), 1+ f(n-c2),…, 1+f(n-ck) )
     int coinChange(vector<int>& coins, int amount) {
-        if(amount<1) return 0;
-        vector<long long int> res(amount+1,INT_MAX);
-        res[0]=0;
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0] = 0;
         for(int i=1; i<=amount; i++) {
             for(int coin:coins) {
-                if(coin<=i && res[i-coin]!=INT_MAX)
-                    res[i] = min(res[i], 1+res[i-coin]);
-            }
+                if(i-coin>=0 && dp[i-coin]!=INT_MAX)
+                    dp[i] = min(dp[i], 1 + dp[i-coin]);
+            } 
         }
-        return res[amount]==INT_MAX? -1:res[amount];
+        return dp[amount]==INT_MAX? -1: dp[amount];
     }
 };
-/*
-  11選1剩10
-    選2剩9
-    選5剩6
-*/
