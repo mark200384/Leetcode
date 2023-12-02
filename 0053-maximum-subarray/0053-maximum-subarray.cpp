d59c1b65a8fb295b42c40df1715433b924185def
Yen <61017020+mark200384@users.noreply.github.com>
2023-12-03 01:15:44 +0800

    Time: 118 ms (5.11%), Space: 68 MB (92.54%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
+    // brute force:O(n^2)
+    // devide & conquer: O(nlogn)
     int maxSubArray(vector<int>& nums) {
-        vector<int> res(nums.size());
-        res[0] = nums[0];
-        for(int i=1; i<nums.size(); i++) {
-            res[i] = max(res[i-1] + nums[i], nums[i]);
-        }
-        
-        return *max_element(res.begin(), res.end());
+        return DevideConquer(nums, 0, nums.size()-1);
     }
+    int DevideConquer(vector<int>& nums, int left, int right) {
+        if(left > right) return INT_MIN;
+        int mid = left - (left - right)/2, leftSum=0, rightSum=0;
+        for(int i = mid-1, curSum = 0; i >= left; i--){
+            curSum += nums[i],
+            leftSum = max(leftSum, curSum);
+        }
+        for(int i=mid+1, curSum=0; i<=right; i++) {
+            curSum += nums[i];
+            rightSum = max(rightSum, curSum);
+        }
+        return max({ DevideConquer(nums, left, mid-1), DevideConquer(nums, mid+1, right), leftSum + nums[mid] + rightSum });
+    }
+    
 };
