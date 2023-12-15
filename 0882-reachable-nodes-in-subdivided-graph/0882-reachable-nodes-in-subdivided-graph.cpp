46e6e8c2193db2cfb8779a855fe15014209d38bc
Yen <61017020+mark200384@users.noreply.github.com>
2023-12-15 17:26:52 +0800

    Time: 147 ms (61.98%), Space: 51.5 MB (56.25%) - LeetHub

--- 0882-reachable-nodes-in-subdivided-graph/0882-reachable-nodes-in-subdivided-graph.cpp
+++ 0882-reachable-nodes-in-subdivided-graph/0882-reachable-nodes-in-subdivided-graph.cpp
@@ -1,47 +1,52 @@
-typedef pair<int,int> PII;
+typedef pair<int,int> PII; 
 
 class Solution {
+    int dist[3000];
+    int resolved[3000];
 public:
     int reachableNodes(vector<vector<int>>& edges, int maxMoves, int n) 
     {
-        unordered_map<int, vector<PII>>Map;
-        for (auto e:edges)
+        vector<PII> adj[3000];  // {nextNode, weight}
+        for (auto e: edges)
         {
-            Map[e[0]].push_back({e[1], e[2]});
-            Map[e[1]].push_back({e[0], e[2]});
+            adj[e[0]].push_back({e[1], e[2]+1});
+            adj[e[1]].push_back({e[0], e[2]+1});
         }
-        unordered_map<int, unordered_map<int,int>>edgeVisitedNodes;
-
-        priority_queue<PII, vector<PII>, greater<>>pq;
-        pq.push({0,0});
-        vector<int>resolved(n);
-
+        
+        priority_queue<PII, vector<PII>, greater<>>pq;  // {dist, nodeIdx}
+        pq.push({0,0});        
+        
         while (!pq.empty())
         {
             auto [d, cur] = pq.top();
             pq.pop();
-
             if (resolved[cur]) continue;
             resolved[cur] = 1;
-
-            for (auto [next, cnt]: Map[cur])
+            dist[cur] = d;
+                        
+            for (auto [next, weight] : adj[cur])
             {
-                // if (resolved[next]) continue;  // Do NOT add this line.
-                edgeVisitedNodes[cur][next] = min(maxMoves - d, cnt);
-                if (maxMoves >= d+cnt+1)
-                    pq.push({d+cnt+1, next});
-            }
+                if (resolved[next]) continue;
+                if (d+weight <= maxMoves)
+                    pq.push({d+weight, next});
+            }            
         }
-
+        
         int count = 0;
         for (auto e: edges)
         {
-            int a = e[0], b = e[1], cnt = e[2];
-            count += min(cnt, edgeVisitedNodes[a][b]+edgeVisitedNodes[b][a]);
+            int a = e[0], b = e[1];
+            int sum = 0;
+            if (resolved[a]) sum += maxMoves - dist[a];
+            if (resolved[b]) sum += maxMoves - dist[b];
+            count += min(sum, e[2]);
         }
+        
         for (int i=0; i<n; i++)
             if (resolved[i])
                 count++;
+        
         return count;
+        
     }
 };
