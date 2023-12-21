3bede4ff96408789fb0d7cc4cbba356c5ca79844
Yen <61017020+mark200384@users.noreply.github.com>
2023-12-21 17:19:15 +0800

    Time: 927 ms (6.73%), Space: 258.6 MB (21.15%) - LeetHub

---
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    //Let DP[i][j] denote the minimum number of white tiles still visible from indices i to floor.length-1 after covering with at most j carpets.
+    int minimumWhiteTiles(string floor, int numCarpets, int carpetLen) {
+        int n = floor.size();
+        floor = "#" + floor;
+        vector<vector<int>> dp(n+1, vector<int>(numCarpets+1, 1e9));
+        dp[0][0]=0;
+        for(int i=1; i<=n; i++) { // # of tiles
+            for(int j=0; j<=numCarpets; j++) { // # of carpets
+                dp[i][j] = INT_MAX/2;
+                dp[i][j] = min(dp[i][j], dp[i-1][j] + (floor[i]=='1'));                               
+                if (j>=1)
+                    dp[i][j] = min(dp[i][j], i>=carpetLen ? dp[i-carpetLen][j-1]:0);
+            }
+        }
+        return dp[n][numCarpets];
+    }
+};
