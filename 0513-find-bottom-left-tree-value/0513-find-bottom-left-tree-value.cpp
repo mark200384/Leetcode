1b03a30dd459808167faaacd73d9fb3826721be6
Yen <61017020+mark200384@users.noreply.github.com>
2024-02-29 02:55:34 +0800

    Time: 4 ms (90.18%), Space: 20 MB (96.27%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -10,23 +10,25 @@
  * };
  */
 class Solution {
-public:
-    //level order
-    int findBottomLeftValue(TreeNode* root) {
-        vector<vector<int>> res;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()) {
-            int sz = q.size();
-            vector<int> level;
-            for(int i=0; i<sz; i++) {
-                auto node = q.front(); q.pop();
-                level.push_back(node->val);
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            res.push_back(level);
+private:
+    int maxDepth;
+    int bottomLeftValue;
+    
+    void dfs(TreeNode* current, int depth) {
+        if(!current) 
+            return;
+        if(depth > maxDepth) {
+            maxDepth = depth;
+            bottomLeftValue = current->val;
         }
-        return res.back()[0];
+        dfs(current->left, depth + 1);
+        dfs(current->right, depth + 1);
+    }
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        maxDepth = -1;
+        bottomLeftValue = 0;
+        dfs(root, 0);
+        return bottomLeftValue;
     }
 };
